e7429faf4d5e871dbf6d816f60fc37a2b4209a2a
robuntu <robuntu@stro.ycdsb.ca>
2019-03-22 09:49:41 -0400

    oop stuff

--- Coin.py
+++ Coin.py
@@ -1,7 +1,25 @@
+import random
+
 # Define your Coin class here
+class Coin():
+    def __init__(self, face):
+        self.x = 1
+        self.y = 2
+
+    def get_face(self):
+
+
+    def flip(self):
+        which_side = random.randint(1,3)
+        return (which_side)
+
+
 
 
 if __name__ == '__main__':
   # put your code that utilizes your Coin class here
+    def main():
+        for x in range (1000):
+
   pass
   
